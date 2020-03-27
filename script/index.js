ec2e0eba3152fb48ada7030c1ac216b183dec84a
Azorji Kelechi Oliver <kelechioliver96@gmail.com>
2020-03-27 22:12:26 +0100

    feat: add post page  - post.html page  - add post.css file  - updated js file
    delivers: 2332884;

--- script/index.js
+++ script/index.js
@@ -1,7 +1,6 @@
 // this is a simple script for clossing and opening the nav bar
 const buggerBtn = document.querySelector('.menu-btn');
 const wrapper = document.querySelector('.wrapper');
-
 buggerBtn.addEventListener('click', () => {
 	return wrapper.classList.toggle('open');
 });
@@ -11,4 +10,7 @@ const pageWidth = window.innerWidth > 0 ? window.innerWidth : screen.width;
 if (pageWidth <= 768) {
 	// this must be a small screen, remove sidebar by default
 	wrapper.classList.add('open');
+
+	// this let's the sidebar go over the content on smaller screen
+	document.querySelector('.main-content').classList.remove('main-content');
 }
