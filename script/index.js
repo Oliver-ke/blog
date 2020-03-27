6d0cfc4b8d3395d39cd755b8ccd6f0bf42419bdd
Azorji Kelechi Oliver <kelechioliver96@gmail.com>
2020-03-27 17:23:09 +0100

    feat: Add footer  - create footer  - add footer styling
     delivers: 0034

--- script/index.js
+++ script/index.js
@@ -2,14 +2,8 @@
 const buggerBtn = document.querySelector('.menu-btn');
 const wrapper = document.querySelector('.wrapper');
 
-let isBarOpen = false;
 buggerBtn.addEventListener('click', () => {
-	if (isBarOpen) {
-		isBarOpen = false;
-		return wrapper.classList.remove('open');
-	}
-	isBarOpen = true;
-	return wrapper.classList.add('open');
+	return wrapper.classList.toggle('open');
 });
 
 // control innitial page width
@@ -17,5 +11,4 @@ const pageWidth = window.innerWidth > 0 ? window.innerWidth : screen.width;
 if (pageWidth <= 768) {
 	// this must be a small screen, remove sidebar by default
 	wrapper.classList.add('open');
-	isBarOpen = true;
 }
